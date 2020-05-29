d1b0aaa99ae0d4f08cc98a1b28af6f2d05e6ce88
Venkatesh Devale <devalevd@amazon.com>
2020-05-29 10:27:21 -0700

    Minor update to AgentConfig

--- agent/src/AgentConfig.js
+++ agent/src/AgentConfig.js
@@ -7,7 +7,7 @@ export const AWS_REGION = 'us-east-1';
 export const INVOKE_URL = '<OUTPUT APIGATEWAY INVOKE URL>';
 
 // Check the ChimeConnectDemoUserAccessKey under the ChimeConnectIntegrationDemo CF template output tab
-export const ACCESS_KEY = 'CHIME CONNECT DEMO USER ACCESS KEY';
+export const ACCESS_KEY = '<CHIME CONNECT DEMO USER ACCESS KEY>';
 
 // Check the ChimeConnectDemoUserSecretKey under the ChimeConnectIntegrationDemo CF template output tab 
-export const SECRET_KEY = 'CHIME CONNECT DEMO USER SECRET KEY';
+export const SECRET_KEY = '<CHIME CONNECT DEMO USER SECRET KEY>';
